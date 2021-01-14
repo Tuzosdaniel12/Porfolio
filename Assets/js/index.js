7d1db8a7720e5261ee250c8429dd56948502a72d
daniel <danielsoledad@gmail.com>
2021-01-14 04:16:51 -0800

    updatinf profile

---
+++ Assets/js/index.js
@@ -0,0 +1,79 @@
+const $heroEl = $('#work') 
+
+let  imagePos = 0;
+let  imagePosS = 7;
+let interval;
+const listOfLinks = [
+    {
+        link:"https://eat-la-burger-please.herokuapp.com/",
+        image:"Assets/images/burger-example.png"
+    },
+    {
+        link:"https://github.com/Tuzosdaniel12/Employee-Tracker",
+        image:"Assets/images/example.png"
+    },{
+        link:"https://tuzosdaniel12.github.io/Horiseon-Marketing/",
+        image:"Assets/images/Horiseon-Marketing.png"
+    },{
+        link:"https://mfrisch87.github.io/MovieTrailerMatchUp/",
+        image:"Assets/images/movie.png"
+    },{
+        link:"https://tuzosdaniel12.github.io/PasswordGenerator/",
+        image:"Assets/images/Password-Generator.png"
+    },{
+        link:"https://tuzosdaniel12.github.io/Day-Scheduler/",
+        image:"Assets/images/schedulerApp.png"
+    },{
+        link:"https://tuzosdaniel12.github.io/Weather-App/",
+        image:"Assets/images/Weather-App.png"
+    },
+];
+
+const startTimer = () =>{
+    console.log("Hit timer")
+    clearInterval(interval);
+    interval = setInterval(timer, 5000);
+}
+
+const timer = ()=>{
+    imagePos+=1;
+    imagePosS-=1;
+    imagePos = checkImagePos(imagePos);
+    imagePosS = checkImagePos(imagePosS);
+
+    $('#first').attr("src", listOfLinks[imagePos].image)
+    $('#link-1').attr("href", listOfLinks[imagePos].link)
+
+    $('#second').attr("src", listOfLinks[imagePosS].image)
+    $('#link-2').attr("href", listOfLinks[imagePosS].link)
+}
+
+function changeImage(e){
+    e.preventDefault();
+    if( $(this).data('target') === "right"){
+        imagePos+=1;
+    }else{
+        imagePos-=1;
+    } 
+    
+    imagePos = checkImagePos(imagePos);
+
+    $('#first').attr("src", listOfLinks[imagePos].image)
+    $('#link-1').attr("href", listOfLinks[imagePos].link)
+}
+
+checkImagePos= (num) =>{
+
+    if(num === -1){
+        return listOfLinks.length -1
+    }else if(imagePos >= listOfLinks.length){
+        console.log("hit", imagePos)
+        return 0
+    }
+    return num
+}
+
+startTimer();
+
+$heroEl.on("click", "[data-target]", changeImage);
+
--- README.md
+++ README.md
@@ -46,35 +46,6 @@ WHEN I resize the page or view the site on various screens and devices
 THEN I am presented with a responsive layout that adapts to my viewport
 ```
 
-## Process
-```
-* 1. started html code and build the structure from top to bottom.
-
-* 2. As I  was building the html structure I was making sure that I used semantic/accessability practices
-
-* 3. Created two css files, one to reset browser initial properties and the second one to style the html
-
-* 4. for this project I decided to use Flex box as much as possible, as well as variables, child selector.
-
-* 5. in the css I broke it in to section, from top to bottom.
-as I finished a section I would move to the next.
-
-* 6. I try to use as descriptive class names and if possible i used the element in html
-
-* 7. As i move down I started to notice all the variables I kept reusing and made one for each term that i kept using.
-
-* 8. When Finish styling css, I started to make the site useable from many platforms as possible, as the size of the screen when down, I slowly started moving percentages,ratios and font sizes down.
-```
-## HTML
-![HTML](https://github.com/Tuzosdaniel12/Porfolio/blob/main/Assets/images/html-code/html-01.png)
-![HTML](https://github.com/Tuzosdaniel12/Porfolio/blob/main/Assets/images/html-code/html-02.png)
-![HTML](https://github.com/Tuzosdaniel12/Porfolio/blob/main/Assets/images/html-code/html-03.png)
-## CSS
-![CSS](https://github.com/Tuzosdaniel12/Porfolio/blob/main/Assets/images/css/css-01.png)
-![CSS](https://github.com/Tuzosdaniel12/Porfolio/blob/main/Assets/images/css/css-02.png)
-![CSS](https://github.com/Tuzosdaniel12/Porfolio/blob/main/Assets/images/css/css-03.png)
-![CSS](https://github.com/Tuzosdaniel12/Porfolio/blob/main/Assets/images/css/css-04.png)
-![CSS](https://github.com/Tuzosdaniel12/Porfolio/blob/main/Assets/images/css/css-05.png)
 ## Links
 * Website
 https://tuzosdaniel12.github.io/Porfolio/
